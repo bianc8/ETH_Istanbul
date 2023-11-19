b18598cc96e49d7c95c6f82db1ded997f0fe15e3
federico-smai <feka7@hotmail.it>
2023-11-19 02:51:51 +0100

    add ccip function

--- app/dapp/schema/page.tsx
+++ app/dapp/schema/page.tsx
@@ -2,19 +2,16 @@
 
 import { useState } from "react";
 
+import sourceCreateSchema from "@/public/SourceMinter.json";
 import Image from "next/image";
 
-import {
-  EAS,
-  Offchain,
-  SchemaEncoder,
-  SchemaRegistry,
-} from "@ethereum-attestation-service/eas-sdk";
+import { SchemaRegistry } from "@ethereum-attestation-service/eas-sdk";
 import { SignerOrProvider } from "@ethereum-attestation-service/eas-sdk/dist/transaction";
-import { ethers } from "ethers";
 import { useWeb3ModalSigner } from "@web3modal/ethers5/react";
+import { ethers } from "ethers";
 
 import pushLogo from "@/public/push-bell.webp";
+import baseLogo from "@/public/Base_Symbol_Blue.svg";
 import toast from "react-hot-toast";
 
 export type IFieldType = {
@@ -29,49 +26,64 @@ const SchemaCreatePage = () => {
       description: "A string can be any text of arbitrary length.",
     },
   ];
-  const [ field1, setField1] = useState<string>("")
-  const [ field2, setField2] = useState<string>("")
-  const [ loading, setLoading] = useState<boolean>(false)
-  
+  const [field1, setField1] = useState<string>("");
+  const [field2, setField2] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
   const { signer } = useWeb3ModalSigner();
 
   const handleCreateSchema = async (event: any) => {
     event?.preventDefault();
     try {
-    setLoading(true)
-    console.log("Start Creating Schema");
+      setLoading(true);
+      console.log("Start Creating Schema");
 
-    const schemaRegistryContractAddress =
-      "0x0a7E2Ff54e76B8E6659aedc9103FB21c038050D0";
-    const schemaRegistry = new SchemaRegistry(schemaRegistryContractAddress);
+      const schemaRegistryContractAddress =
+        "0x0a7E2Ff54e76B8E6659aedc9103FB21c038050D0";
+      const schemaRegistry = new SchemaRegistry(schemaRegistryContractAddress);
+      schemaRegistry.connect(signer as unknown as SignerOrProvider);
+      const schema = "string " + field1 + ", string " + field2;
+      const resolverAddress = "0xB4Fb406b75db78D69c28E616Ef317f6ea6FE3497";
+      const revocable = true;
 
-    schemaRegistry.connect(signer as unknown as SignerOrProvider);
+      const transaction = await schemaRegistry.register({
+        schema,
+        resolverAddress,
+        revocable,
+      });
 
-    const schema = "string "+field1+", string "+field2;
-    const resolverAddress = "0xB4Fb406b75db78D69c28E616Ef317f6ea6FE3497"; //0xB4Fb406b75db78D69c28E616Ef317f6ea6FE3497"; // Smart Contract
-    const revocable = true;
+      const resolverAddressBase = "0xc5ed581f35741340b4804cef076adc5c9c46a872";
+      const ccip = new ethers.Contract(
+        "0x0e5b100a2890c43fab0c1b67183bf27399379692",
+        new ethers.utils.Interface(sourceCreateSchema.abi),
+        signer
+      );
+      const tx = await ccip.register(
+        "5790810961207155433",
+        "0x099ca1f3b90210735b880dba7bea82d88b3cb409",
+        0,
+        schema,
+        resolverAddressBase,
+        true
+      );
+      console.log(tx);
 
-    const transaction = await schemaRegistry.register({
-      schema,
-      resolverAddress,
-      revocable,
-    });
-
-    // Optional: Wait for transaction to be validated
-    const result = await transaction.wait();
-    toast.success("Schema Created")
-    console.log("Schema Created:", result);
-    setField1("")
-    setField2("")
-  }
-  catch(e) {
-    console.error(e)
-    toast.error("Ooops, something went wrong...")
-  }
-  finally {
-    setLoading(false)
-  }
-    
+      const result = await transaction.wait();
+      toast.success("Schema Created");
+      console.log("Schema Created:", result);
+      const result_ccip = await tx.wait();
+      toast.success(
+        "Schema sent to CCIP. This may take a while before is deployed on Base"
+      );
+      console.log("CCIP Created:", result_ccip);
+      setField1("");
+      setField2("");
+    } catch (e) {
+      console.error(e);
+      toast.error("Ooops, something went wrong...");
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -99,7 +111,6 @@ const SchemaCreatePage = () => {
             ))}
           </select>
         </div>
-
 
         <div className="flex flex-row gap-4">
           <input
@@ -135,6 +146,19 @@ const SchemaCreatePage = () => {
             />
           </p>
         </div>
+
+        <div className="flex flex-row space-x-2 items-center">
+          <input
+            type="checkbox"
+            className="toggle toggle-md toggle-success"
+            checked
+          />
+          <p className="flex flex-row text-lg space-x-1">
+            <span>Deploy on Base</span>
+            <Image src={baseLogo} width={25} height={25} alt="Base Logo" />
+          </p>
+        </div>
+
         <button type="submit" className="btn justify-center">
           {loading ? "Loading..." : "Submit"}
         </button>
